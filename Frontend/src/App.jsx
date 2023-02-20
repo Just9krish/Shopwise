553cdc7a79b0f8ae907919535ed7f7c0cdc67b2f
just9krish <rvamit2648@gmail.com>
2023-02-20 20:45:43 +0530

    header section of page completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import About from "./Pages/About";
+import Cart from "./Pages/Cart";
+import Contact from "./Pages/Contact";
+import Error from "./Pages/Error";
+import Home from "./Pages/Home";
+import Product from "./Pages/Product";
+import Products from "./Pages/Products";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,95 @@
+import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.png";
+import menuClose from "../assets/close-icon.svg";
+import menuIcon from "../assets/menu-icon.svg";
+import cartIcon from "../assets/cart-icon.svg";
+import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose, AiOutlineShoppingCart } from "react-icons/ai";
+
+export default function Header() {
+  const links = [
+    { path: "/", name: "Home", id: 1 },
+    { path: "/products", name: "Products", id: 2 },
+    { path: "/about", name: "About", id: 3 },
+    { path: "/contact", name: "Contact", id: 4 },
+  ];
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      {/* for mobile navigation */}
+      <header className="bg-white w-full z-40 lg:hidden">
+        <div className="flex justify-between items-center px-6 py-4">
+          <div
+            className="z-40 cursor-pointer "
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            {isOpen ? (
+              <AiOutlineClose color="white" fontSize="1.25em" />
+            ) : (
+              <GiHamburgerMenu />
+            )}
+          </div>
+          <NavLink>
+            <img src={logo} className="h-6" alt="Company logo" />
+          </NavLink>
+          <div className="relative cursor-pointer">
+            <img src={cartIcon} alt="Cart" />
+            <div className="absolute bg-orange-400 text-white text-[9px] flex justify-center items-center rounded-full p-1 h-3 w-3 -top-1 -right-1">
+              10
+            </div>
+          </div>
+        </div>
+
+        <div
+          className={`bg-orange-400 w-4/5 z-30 h-screen fixed top-0 flex flex-col items-start shadow-xl transition-all duration-500 ${
+            isOpen ? "left-0" : "-left-[100%]"
+          }`}
+        >
+          <ul
+            className={`flex flex-col mt-24 space-y-6 pl-6 text-white font-semibold 
+            }`}
+          >
+            {links.map((link) => (
+              <li className="hover:text-gray-200 transition-all">
+                <NavLink key={link.id} to={link.path}>
+                  {link.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </header>
+
+      {/* for desktop navigation */}
+      <header className="hidden lg:block w-full px-28">
+        <div className="flex justify-between items-center p-6 border-b">
+          <NavLink to="/">
+            <img src={logo} className="h-7" alt="Logo" />
+          </NavLink>
+          <div className="flex items-center space-x-8">
+            {links.map((link) => (
+              <NavLink
+                className="block relative hover:after:absolute after:top-12 after:h-1 after:inset-x-0 after:w-full after:bg-orange-500 after:transition-all after:duration-300"
+                key={link.id}
+                to={link.path}
+              >
+                {link.name}
+              </NavLink>
+            ))}
+            <div className="relative">
+              <NavLink to="/cart">
+                <AiOutlineShoppingCart fontSize="1.2em" />
+              </NavLink>
+              <div className="absolute bg-orange-500 text-white text-[8px] flex justify-center items-center rounded-full p-1 h-3 w-3 -top-1 -right-1">
+                10
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
