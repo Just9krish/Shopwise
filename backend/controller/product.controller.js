a3f4352f27bfe4b0854b4703d70f545f0c36fe2f
just9krish <rvamit2648@gmail.com>
2023-02-24 13:07:44 +0530

    working with backend

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -1,14 +1,41 @@
 const Product = require("../models/product.model");
+const isValid = require("mongoose").Types.ObjectId.isValid;
 
 async function getAllProducts(req, res, next) {
   try {
     const products = await Product.find();
     res.status(200).json({ success: true, data: products });
   } catch (err) {
-    res.status(400).json({ success: false, error: err.message });
+    res.status(500).json({ success: false, message: err.message });
+  }
+}
+
+async function getProduct(req, res) {
+  try {
+    const id = req.params.id;
+
+    if (!isValid(id)) {
+      res.status(406).json({
+        success: false,
+        message: "Product is not found with given Id",
+      });
+    }
+    const product = await Product.findOne({ id });
+
+    if (!product) {
+      res.status(404).json({
+        success: false,
+        message: "Product is not found with given id",
+      });
+    }
+
+    res.status(200).json({ success: true, data: product });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
   }
 }
 
 module.exports = {
   getAllProducts,
+  getProduct,
 };
